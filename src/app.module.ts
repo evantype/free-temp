17c1c007dac84347cd265254d2f8b3c129b59b9f
Evgeniy Antipov <evantype@gmail.com>
2022-08-07 13:42:11 +0300

    db connect throw env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import {Module} from "@nestjs/common"
 import {SequelizeModule} from "@nestjs/sequelize";
+import { UsersModule } from './users/users.module';
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
     controllers: [],
@@ -7,13 +9,17 @@ import {SequelizeModule} from "@nestjs/sequelize";
     imports: [
         SequelizeModule.forRoot({
             dialect: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: 'postgres',
-            database: 'free-temp',
+            host: process.env.POSTGRES_HOST,
+            port: Number(process.env.POSTGRES_PORT),
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DB,
             models: [],
         }),
+        UsersModule,
+        ConfigModule.forRoot({
+            envFilePath: '.env'
+        })
     ]
 })
 export class AppModule {}
